7a00cdbe0280d313f5cb33bfc5c38ea8d7f6a5cb
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-03 06:56:46 +0800

    Update bh-mj-form-footer component.

---
+++ component.jsx
@@ -0,0 +1,14 @@
+
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import FormFooter from "./form-footer.js";
+
+ReactDOM.render( 
+	<FormFooter
+		label="This is a form footer">
+	</FormFooter>,
+	document.getElementById( "root" ) 
+);
--- test.jsx
+++
@@ -1,9 +0,0 @@
-
-"use strict";
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import FormFooter from "./form-footer.js";
-
-ReactDOM.render( <FormFooter></FormFooter> );
